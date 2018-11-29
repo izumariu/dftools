0a43adb6e2b93de8fd6da21098889cd899bf1f05
izumariu <marius.wuerker@t-online.de>
2018-11-29 14:11:18 +0100

    INIT

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,13 @@ use std::process;
 
 fn main() {
 
-    if is_root() {
-        eprintln!("You are root!");
-    } else {
-        eprintln!("You are not root.");
+    let target_dir = get_target_dir();
+
+    for font in &get_fontnames() {
+        println!("'{}' ==[to_urlname]=> {}", font, to_urlname(&font));
     }
 
-    {
-
-        let mut first_flag = true;
-
-        for arg in env::args() {
-
-            if first_flag {
-                first_flag = false;
-                continue;
-            }
-
-            println!("{}'' ==[to_urlname]=> {}", arg, to_urlname(&arg));
-            
-        }
-
-    }
+    eprintln!("{:?}", target_dir);
 
 }
 
@@ -44,6 +29,61 @@ fn is_root() -> bool {
         },
 
     }
+
+}
+
+fn get_target_dir() -> String {
+
+    #[cfg(target_os = "linux")]
+    let mut target_dir = String::from("share/fonts");
+
+    #[cfg(target_os = "macos")]
+    let mut target_dir = String::from("Library/Fonts");
+
+    let mut final_dir = String::new();
+
+    if is_root() {
+
+        #[cfg(target_os = "linux")]
+        target_dir.insert_str(0, "/usr/");
+
+        #[cfg(target_os = "macos")]
+        target_dir.insert_str(0, "/");
+
+    } else {
+
+        #[cfg(target_os = "linux")]
+        final_dir.push_str(&format!("/home/{}/.local/", env::var("USER").unwrap()));
+
+        #[cfg(target_os = "macos")]
+        final_dir.push_str(&format!("/Users/{}/", env::var("USER").unwrap()));
+
+    }
+
+    final_dir.push_str(&target_dir);
+
+    final_dir
+
+}
+
+fn get_fontnames() -> Vec<String> {
+
+    let mut outvec:Vec<String> = Vec::new();
+
+    let mut first_flag = true;
+
+    for arg in env::args() {
+
+        if first_flag {
+            first_flag = false;
+            continue;
+        }
+
+        outvec.push(arg);
+
+    }
+
+    outvec
 
 }
 
